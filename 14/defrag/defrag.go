589c265dcebad46dee52fcf007815dabc7c8f328
Kieron Browne <kbrowne@pivotal.io>
2017-12-15 17:50:16 +0000

    14 part 2

--- 14/defrag/defrag.go
+++ 14/defrag/defrag.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/kieron-pivotal/advent2017/10/hash"
+	"github.com/kieron-pivotal/advent2017/12/graph"
 )
 
 func Hash(seed string, row int) string {
@@ -34,4 +35,42 @@ func CountUsed(in string) int {
 		c += BitCount(Hash(in, i))
 	}
 	return c
+}
+
+func HexToBin(in string) string {
+	out := ""
+	bs, _ := hex.DecodeString(in)
+	for _, b := range bs {
+		out += fmt.Sprintf("%08b", b)
+	}
+	return out
+}
+
+func HashesToForest(hashes []string) *graph.Graph {
+	g := graph.New()
+	for n := 0; n < 128*128; n++ {
+		r := n / 128
+		c := n % 128
+		if hashes[r][c] == '0' {
+			continue
+		}
+		children := []int{}
+		if c < 127 && hashes[r][c+1] == '1' {
+			children = append(children, n+1)
+		}
+		if r < 127 && hashes[r+1][c] == '1' {
+			children = append(children, n+128)
+		}
+		g.LinkNodes(n, children)
+	}
+	return g
+}
+
+func CountBlocks(in string) int {
+	hashes := []string{}
+	for i := 0; i < 128; i++ {
+		hashes = append(hashes, HexToBin(Hash(in, i)))
+	}
+	g := HashesToForest(hashes)
+	return g.Groups()
 }
