9e3b11c00966649cf3f201f3ef7c8b9d367e717e
Kieron Browne <kbrowne@pivotal.io>
2017-12-17 17:56:30 +0000

    17 part 2 - though it took 10mins to run :(

--- 17/linkedlist/linkedlist.go
+++ 17/linkedlist/linkedlist.go
@@ -1,36 +1,42 @@
 package linkedlist
 
+type List struct {
+	Start  *ListItem
+	Len    int
+	CurPos *ListItem
+}
+
 type ListItem struct {
 	Val  int
 	Next *ListItem
 }
 
-func NewCiruclarList(val int) *ListItem {
+func NewCiruclarList(val int) *List {
+	list := List{Len: 1}
 	li := ListItem{Val: val}
 	li.Next = &li
-	return &li
+	list.Start = &li
+	list.CurPos = &li
+	return &list
 }
 
-func (li *ListItem) Insert(val int) *ListItem {
+func (l *List) Insert(val int) {
 	new := ListItem{Val: val}
-	new.Next = li.Next
-	li.Next = &new
-	return &new
+	new.Next = l.CurPos.Next
+	l.CurPos.Next = &new
+	l.CurPos = &new
+	l.Len++
 }
 
-func (li *ListItem) Advance(places int) *ListItem {
-	ret := li
-	for i := 0; i < places; i++ {
-		ret = ret.Next
+func (l *List) Advance(places int) {
+	for i := 0; i < places%l.Len; i++ {
+		l.CurPos = l.CurPos.Next
 	}
-	return ret
 }
 
-func (li *ListItem) SkipAndInsert(skipSize, iterations int) *ListItem {
-	cur := li
+func (l *List) SkipAndInsert(skipSize, iterations int) {
 	for i := 0; i < iterations; i++ {
-		cur = cur.Advance(skipSize)
-		cur = cur.Insert(i + 1)
+		l.Advance(skipSize)
+		l.Insert(i + 1)
 	}
-	return cur.Next
 }
