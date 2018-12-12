abb619856ab10c0d5d5a73fb568a57ddc87872e4
Kieron Browne <kbrowne@pivotal.io>
2018-12-12 20:52:01 +0000

    q12

--- 2018/q12/lib.go
+++ 2018/q12/lib.go
@@ -3,22 +3,54 @@ package q12
 import (
 	"bufio"
 	"bytes"
+	"fmt"
 	"io"
 	"log"
 	"strings"
 )
 
 type Plants struct {
-	state map[int]bool
-	rules [][5]bool
-	min   int
-	max   int
+	state     map[int]rune
+	rules     map[string]string
+	min       int
+	max       int
+	iteration int
+}
+
+func (p *Plants) Step() {
+	p.iteration++
+	newState := map[int]rune{}
+	newMin := p.max
+	newMax := p.min
+	for i := p.min - 5; i <= p.max+1; i++ {
+		var buf bytes.Buffer
+		for j := 0; j < 5; j++ {
+			r, ok := p.state[i+j]
+			if !ok {
+				r = rune('.')
+			}
+			buf.WriteString(string(r))
+		}
+		match := buf.String()
+		if p.rules[match] == "#" {
+			newState[i+2] = rune('#')
+			if i+2 < newMin {
+				newMin = i + 2
+			}
+			newMax = i + 2
+		} else {
+			newState[i+2] = rune('.')
+		}
+	}
+	p.state = newState
+	p.min = newMin
+	p.max = newMax
 }
 
 func NewPlants(r io.Reader) *Plants {
 	p := Plants{}
-	p.state = map[int]bool{}
-	p.rules = [][5]bool{}
+	p.state = map[int]rune{}
+	p.rules = map[string]string{}
 
 	br := bufio.NewReader(r)
 	line, err := br.ReadString('\n')
@@ -44,18 +76,28 @@ func NewPlants(r io.Reader) *Plants {
 	return &p
 }
 
+func (p *Plants) HashPosSum() int {
+	sum := 0
+	for i, r := range p.state {
+		if r == rune('#') {
+			sum += i
+		}
+	}
+	return sum
+}
+
 func (p *Plants) LoadRule(rule string) {
 	if !strings.Contains(rule, "=>") {
 		return
 	}
-
+	var matchStr, resultStr string
+	fmt.Sscanf(rule, "%s => %s", &matchStr, &resultStr)
+	p.rules[matchStr] = resultStr
 }
 
 func (p *Plants) LoadState(state string) {
 	for i, r := range state {
-		if r == rune('#') {
-			p.state[i] = true
-		}
+		p.state[i] = r
 		p.max = i
 	}
 }
@@ -63,15 +105,11 @@ func (p *Plants) LoadState(state string) {
 func (p *Plants) State() string {
 	var buf bytes.Buffer
 	for i := p.min; i < p.max+1; i++ {
-		if p.state[i] {
-			buf.WriteString("#")
-		} else {
-			buf.WriteString(".")
-		}
+		buf.WriteString(string(p.state[i]))
 	}
 	return buf.String()
 }
 
-func (p *Plants) Rules() []string {
-	return []string{}
+func (p *Plants) Rules() map[string]string {
+	return p.rules
 }
--- 2018/q12/q12_test.go
+++ 2018/q12/q12_test.go
@@ -1,7 +1,6 @@
 package q12_test
 
 import (
-	"io"
 	"os"
 
 	"github.com/kieron-pivotal/advent2017/2018/q12"
@@ -11,17 +10,21 @@ import (
 
 var _ = Describe("Q12", func() {
 
+	var (
+		r *os.File
+	)
+
+	BeforeEach(func() {
+		var err error
+		r, err = os.Open("example")
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		r.Close()
+	})
+
 	Context("loading config", func() {
-		var (
-			r io.Reader
-		)
-
-		BeforeEach(func() {
-			var err error
-			r, err = os.Open("example")
-			Expect(err).NotTo(HaveOccurred())
-		})
-
 		It("can get initial state", func() {
 			plants := q12.NewPlants(r)
 			Expect(plants.State()).To(Equal("#..#.#..##......###...###"))
@@ -29,7 +32,35 @@ var _ = Describe("Q12", func() {
 
 		It("can load rules", func() {
 			plants := q12.NewPlants(r)
-			Expect(plants.Rules()).To(HaveLen(14))
+			rules := plants.Rules()
+			Expect(rules).To(HaveLen(14))
+			Expect(rules["..#.."]).To(Equal("#"))
 		})
+	})
+
+	Context("evolving", func() {
+		It("can evolve some steps", func() {
+			plants := q12.NewPlants(r)
+			By("step 1", func() {
+				plants.Step()
+				Expect(plants.State()).To(Equal("#...#....#.....#..#..#..#"))
+			})
+			By("step 2", func() {
+				plants.Step()
+				Expect(plants.State()).To(Equal("##..##...##....#..#..#..##"))
+			})
+			By("step 3", func() {
+				plants.Step()
+				Expect(plants.State()).To(Equal("#.#...#..#.#....#..#..#...#"))
+			})
+		})
+	})
+
+	It("has correct hash sum after 20 steps", func() {
+		plants := q12.NewPlants(r)
+		for i := 0; i < 20; i++ {
+			plants.Step()
+		}
+		Expect(plants.HashPosSum()).To(Equal(325))
 	})
 })
