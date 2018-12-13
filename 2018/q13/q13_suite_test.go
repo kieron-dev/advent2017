ed2cf5fac5b0d2b1c7a2bef1f4ef90f47e5a6059
Kieron Browne <kbrowne@pivotal.io>
2018-12-13 18:55:11 +0000

    q13 part A

---
+++ 2018/q13/q13_suite_test.go
@@ -0,0 +1,13 @@
+package q13_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestQ13(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Q13 Suite")
+}
