9dfe27972451c07b0e174841e8bbb6edcda8d8b3
Kieron Browne <kbrowne@pivotal.io>
2018-12-22 22:17:44 +0000

    q17
    FINALLY!!!

---
+++ 2018/q17/partA/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/kieron-pivotal/advent2017/2018/q17"
+)
+
+func main() {
+	f, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := q17.NewSlice(f)
+	s.Flow(q17.NewCoord(500, 0))
+	s.Print()
+	fmt.Printf("s.CountWater() = %+v\n", s.CountWater())
+}
