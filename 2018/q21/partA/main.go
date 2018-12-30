171b3d88694998578401198b6edb9c97c250fbe3
Kieron Browne <kbrowne@pivotal.io>
2018-12-30 09:33:08 +0000

    decent solution for q21

--- 2018/q21/partA/main.go
+++ 2018/q21/partA/main.go
@@ -2,50 +2,25 @@ package main
 
 import "fmt"
 
-func main() {
-	mn := 270
-	mr0 := 0
-	for r0 := 0; r0 < 256000000; r0++ {
-		n := doIt(r0, mn)
-		if n > 0 && n < mn {
-			mn = n
-			mr0 = r0
-			fmt.Printf("mn = %+v\n", mn)
-			fmt.Printf("mr0 = %+v\n", mr0)
-		}
-	}
-	fmt.Println("-------")
-	fmt.Printf("mn = %+v\n", mn)
-	fmt.Printf("mr0 = %+v\n", mr0)
-
-}
-
 const r3const = 4921097
 
-func doIt(r0, mn int) int {
+func main() {
 	r1 := 1 << 16
 	r3 := r3const
-	inst := 0
 
 	for {
-		inst++
-		if inst > mn {
-			return -1
-		}
 		r3 += r1 & 255
 		r3 &= (1 << 24) - 1
 		r3 *= 65899
 		r3 &= (1 << 24) - 1
 
 		if r1 < 256 {
-			if r3 == r0 {
-				return inst
-			}
-			r1 = r3 | 1<<16
-			r3 = r3const
+			fmt.Printf("r3 = %+v\n", r3)
+			return
+			// r1 = r3 | 1<<16
+			// r3 = r3const
 		} else {
 			r1 /= 256
-			inst += r1
 		}
 	}
 }
