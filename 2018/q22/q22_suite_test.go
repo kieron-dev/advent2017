80ec810fe965dba2d699c081ff49eafca8fb1f2f
Kieron Browne <kbrowne@pivotal.io>
2018-12-30 19:57:34 +0000

    q22 done.

---
+++ 2018/q22/q22_suite_test.go
@@ -0,0 +1,13 @@
+package q22_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestQ22(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Q22 Suite")
+}
