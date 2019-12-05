0d5f75e8ee5f3d3f2c2c02fb92ed633c5fe6477f
Kieron Browne <kbrowne@pivotal.io>
2019-12-05 21:03:31 +0000

    q05

--- 2019/computer.go
+++ 2019/computer.go
@@ -1,17 +1,22 @@
 package advent2019
 
 import (
+	"fmt"
+	"io"
 	"strconv"
 	"strings"
 )
 
 type Computer struct {
-	registers []int
+	registers   []int
+	inputReader io.Reader
 }
 
-func NewComputer() *Computer {
-	c := Computer{}
-	c.registers = []int{}
+func NewComputer(inputReader io.Reader) *Computer {
+	c := Computer{
+		inputReader: inputReader,
+		registers:   []int{},
+	}
 	return &c
 }
 
@@ -32,17 +37,78 @@ func (c *Computer) Prime(noun, verb int) {
 
 func (c *Computer) Calculate() int {
 	pos := 0
+	var jump int
 	for {
-		switch c.registers[pos] {
+		switch c.registers[pos] % 100 {
 		case 1:
-			c.registers[c.registers[pos+3]] = c.registers[c.registers[pos+1]] + c.registers[c.registers[pos+2]]
+			c.registers[c.registers[pos+3]] = c.ValueAt(pos, 1) + c.ValueAt(pos, 2)
+			jump = 4
 		case 2:
-			c.registers[c.registers[pos+3]] = c.registers[c.registers[pos+1]] * c.registers[c.registers[pos+2]]
+			c.registers[c.registers[pos+3]] = c.ValueAt(pos, 1) * c.ValueAt(pos, 2)
+			jump = 4
+		case 3:
+			c.registers[c.registers[pos+1]] = c.readInput()
+			jump = 2
+		case 4:
+			fmt.Printf("--- %d\n", c.ValueAt(pos, 1))
+			jump = 2
+		case 5:
+			if c.ValueAt(pos, 1) != 0 {
+				pos = c.ValueAt(pos, 2)
+				jump = 0
+			} else {
+				jump = 3
+			}
+		case 6:
+			if c.ValueAt(pos, 1) == 0 {
+				pos = c.ValueAt(pos, 2)
+				jump = 0
+			} else {
+				jump = 3
+			}
+		case 7:
+			if c.ValueAt(pos, 1) < c.ValueAt(pos, 2) {
+				c.registers[c.registers[pos+3]] = 1
+			} else {
+				c.registers[c.registers[pos+3]] = 0
+			}
+			jump = 4
+		case 8:
+			if c.ValueAt(pos, 1) == c.ValueAt(pos, 2) {
+				c.registers[c.registers[pos+3]] = 1
+			} else {
+				c.registers[c.registers[pos+3]] = 0
+			}
+			jump = 4
 		case 99:
 			return c.registers[0]
 		}
-		pos += 4
+		pos += jump
 	}
+}
+
+func (c *Computer) readInput() int {
+	var input int
+	fmt.Print("> ")
+	n, err := fmt.Fscanf(c.inputReader, "%d", &input)
+	if err != nil {
+		panic(err)
+	}
+	if n != 1 {
+		panic("expected to read an int")
+	}
+	return input
+}
+
+func (c *Computer) ValueAt(base, offset int) int {
+	mask := 100
+	for i := 1; i < offset; i++ {
+		mask *= 10
+	}
+	if (c.registers[base]/mask)%10 == 1 {
+		return c.registers[base+offset]
+	}
+	return c.registers[c.registers[base+offset]]
 }
 
 func (c *Computer) TryCalculate() (ret int) {
