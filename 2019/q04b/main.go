e27d281d9de597ba1b7b525f955f47bc409b5094
Kieron Browne <kbrowne@pivotal.io>
2019-12-05 12:05:46 +0000

    Reset numbers to the right more efficiently

--- 2019/q04b/main.go
+++ 2019/q04b/main.go
@@ -9,13 +9,11 @@ import (
 func main() {
 	gen := advent2019.NewPWGen(347312, 805915, true)
 	i := 1
-	fmt.Printf("gen.Current() = %+v\n", gen.Current())
 	for {
 		_, err := gen.Next()
 		if err != nil {
 			break
 		}
-		fmt.Printf("gen.Current() = %+v\n", gen.Current())
 		i++
 	}
 	fmt.Printf("res = %d\n", i)
