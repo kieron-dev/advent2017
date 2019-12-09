71a22aeacb4975da94af358de44dba85fe4364e5
Kieron Browne <kbrowne@pivotal.io>
2019-12-09 22:39:12 +0000

    refactor day 3

---
+++ 2019/days/q03_test.go
@@ -0,0 +1,49 @@
+package days_test
+
+import (
+	"os"
+
+	"github.com/kieron-pivotal/advent2017/advent2019"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Q03", func() {
+	var (
+		file       *os.File
+		fileReader advent2019.FileReader
+		grids      []*advent2019.Grid
+	)
+
+	BeforeEach(func() {
+		var err error
+		file, err = os.Open("./input03")
+		if err != nil {
+			panic(err)
+		}
+		fileReader = advent2019.FileReader{}
+		grids = []*advent2019.Grid{}
+	})
+
+	It("does part A", func() {
+		fileReader.Each(file, func(line string) {
+			grid := advent2019.NewGrid()
+			grid.Move(line)
+			grids = append(grids, grid)
+		})
+
+		res := grids[0].ClosestCommonDist(grids[1])
+		Expect(res).To(Equal(1626))
+	})
+
+	It("does part B", func() {
+		fileReader.Each(file, func(line string) {
+			grid := advent2019.NewGrid()
+			grid.Move(line)
+			grids = append(grids, grid)
+		})
+
+		res := grids[0].QuickestCommonCell(grids[1])
+		Expect(res).To(Equal(27330))
+	})
+})
