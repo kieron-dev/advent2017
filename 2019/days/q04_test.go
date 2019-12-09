559831fa1d6c9acfe907c22f38dab0b644abc883
Kieron Browne <kbrowne@pivotal.io>
2019-12-09 22:52:18 +0000

    refactored 4 and 5

---
+++ 2019/days/q04_test.go
@@ -0,0 +1,33 @@
+package days_test
+
+import (
+	"github.com/kieron-pivotal/advent2017/advent2019"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Q04", func() {
+	It("does part A", func() {
+		gen := advent2019.NewPWGen(347312, 805915, false)
+		var err error
+		i := 0
+		for err == nil {
+			_, err = gen.Next()
+			i++
+		}
+		Expect(i).To(Equal(594))
+	})
+
+	It("does part B", func() {
+		gen := advent2019.NewPWGen(347312, 805915, true)
+		i := 1
+		for {
+			_, err := gen.Next()
+			if err != nil {
+				break
+			}
+			i++
+		}
+		Expect(i).To(Equal(364))
+	})
+})
