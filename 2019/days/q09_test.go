93d9b71a285c8c8330a8f5adf17c8b38816946dc
Kieron Browne <kbrowne@pivotal.io>
2019-12-09 23:18:29 +0000

    refactor day 9

--- 2019/days/q09_test.go
+++ 2019/days/q09_test.go
@@ -1,16 +1,54 @@
 package days_test
 
 import (
+	"io/ioutil"
+	"math/big"
+	"strings"
+
+	"github.com/kieron-pivotal/advent2017/advent2019"
 	. "github.com/onsi/ginkgo"
-	// . "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Q09", func() {
-	It("does part A", func() {
+	var (
+		progBytes []byte
+		in, out   chan big.Int
+		c         *advent2019.Computer
+	)
 
+	BeforeEach(func() {
+		var err error
+		progBytes, err = ioutil.ReadFile("./input09")
+		if err != nil {
+			panic(err)
+		}
+
+		in = make(chan big.Int, 1)
+		out = make(chan big.Int, 20)
+
+		c = advent2019.NewComputer(in, out)
+		c.SetInput(strings.TrimSpace(string(progBytes)))
+	})
+
+	It("does part A", func() {
+		in <- *big.NewInt(1)
+		c.Calculate()
+
+		close(out)
+		var last string
+		for n := range out {
+			last = n.String()
+		}
+		Expect(last).To(Equal("3454977209"))
 	})
 
 	It("does part B", func() {
+		in <- *big.NewInt(2)
+		c.Calculate()
 
+		close(out)
+		res := <-out
+		Expect(res.String()).To(Equal("50120"))
 	})
 })
