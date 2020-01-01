2fd312a2daf7df5f1d5505b2d89ae6a8fa420db7
Kieron Browne <kbrowne@pivotal.io>
2020-01-01 21:43:36 +0000

    Create a graph from the keys and doors
    See if it improves performance

--- 2019/manyworlds/maze.go
+++ 2019/manyworlds/maze.go
@@ -11,15 +11,24 @@ import (
 	"github.com/kieron-pivotal/advent2017/advent2019/grid"
 )
 
+type graphNode struct {
+	label  rune
+	coord  grid.Coord
+	linked map[rune]int
+	dist   int
+}
+
 type World struct {
-	grid     map[grid.Coord]rune
-	keyCount int
-	startPos grid.Coord
+	grid       map[grid.Coord]rune
+	keyCount   int
+	startPos   grid.Coord
+	graphNodes map[rune]*graphNode
 }
 
 func NewWorld() *World {
 	w := World{}
 	w.grid = map[grid.Coord]rune{}
+	w.graphNodes = map[rune]*graphNode{}
 
 	return &w
 }
@@ -42,6 +51,93 @@ func (w *World) LoadMap(r io.Reader) {
 		}
 		row++
 	})
+
+	w.makeGraph()
+}
+
+func (w *World) getConnectedNodes(from *graphNode) []*graphNode {
+	visited := map[grid.Coord]bool{}
+	stack := []grid.Coord{from.coord}
+
+	distances := map[grid.Coord]int{from.coord: 0}
+
+	connected := []*graphNode{}
+
+	for len(stack) > 0 {
+		curPos := stack[0]
+		stack = stack[1:]
+
+		if visited[curPos] {
+			continue
+		}
+		visited[curPos] = true
+
+		if curPos != from.coord && w.grid[curPos] != '.' {
+			connected = append(connected, &graphNode{
+				coord: curPos,
+				label: w.grid[curPos],
+				dist:  distances[curPos],
+			})
+			continue
+		}
+
+		for _, dir := range []grid.Direction{grid.North, grid.South, grid.East, grid.West} {
+			nextPos := curPos.Move(dir)
+			cell := w.grid[nextPos]
+			switch {
+			case cell == '#':
+				continue
+			default:
+				stack = append(stack, nextPos)
+				d, ok := distances[nextPos]
+				if !(ok && d > distances[curPos]+1) {
+					distances[nextPos] = distances[curPos] + 1
+				}
+			}
+		}
+	}
+
+	return connected
+}
+
+func (w *World) makeGraph() {
+	start := &graphNode{
+		label:  '0',
+		coord:  w.startPos,
+		linked: map[rune]int{},
+	}
+
+	stack := []*graphNode{start}
+
+	visited := map[rune]bool{}
+
+	for len(stack) > 0 {
+		cur := stack[0]
+		stack = stack[1:]
+
+		if visited[cur.label] {
+			continue
+		}
+		visited[cur.label] = true
+
+		cur.linked = map[rune]int{}
+		for _, conn := range w.getConnectedNodes(cur) {
+			cur.linked[conn.label] = conn.dist
+			stack = append(stack, conn)
+		}
+
+		w.graphNodes[cur.label] = cur
+	}
+
+}
+
+func (w *World) PrintGraph() {
+	for _, n := range w.graphNodes {
+		fmt.Printf("node %q\n", n.label)
+		for l, d := range n.linked {
+			fmt.Printf("\t-> %q: %d\n", l, d)
+		}
+	}
 }
 
 func (w *World) KeysCount() int {
@@ -59,7 +155,7 @@ func (w *World) CharAt(c grid.Coord) rune {
 type path struct {
 	steps int
 	keys  map[rune]bool
-	pos   grid.Coord
+	pos   rune
 }
 
 func (p path) ToString() string {
@@ -69,7 +165,7 @@ func (p path) ToString() string {
 	}
 	sort.Strings(keySlice)
 	keys := strings.Join(keySlice, "")
-	return fmt.Sprintf("(%d,%d):%s", p.pos.X(), p.pos.Y(), keys)
+	return fmt.Sprintf("%c:%s", p.pos, keys)
 }
 
 func (w *World) MinStepsToCollectKeys() int {
@@ -79,61 +175,54 @@ func (w *World) MinStepsToCollectKeys() int {
 		path{
 			steps: 0,
 			keys:  nil,
-			pos:   w.startPos,
+			pos:   '0',
 		},
 	}
 
-	visited := map[string]bool{}
+	visited := map[string]int{}
 
 	for len(stack) > 0 {
 		curPath := stack[0]
 		stack = stack[1:]
 
-		curPathKey := curPath.ToString()
-		if visited[curPathKey] {
+		if curPath.steps >= min {
 			continue
 		}
-		visited[curPathKey] = true
 
-		for _, dir := range []grid.Direction{grid.North, grid.South, grid.West, grid.East} {
-			nextPos := curPath.pos.Move(dir)
-			nextCell := w.grid[nextPos]
-			switch {
-			case nextCell == '.':
-				stack = append(stack, path{
-					steps: curPath.steps + 1,
-					keys:  curPath.keys,
-					pos:   nextPos,
-				})
-			case nextCell == '#':
-				continue
-			case nextCell >= 'a' && nextCell <= 'z':
-				newKeys := map[rune]bool{}
-				for k, v := range curPath.keys {
-					newKeys[k] = v
-				}
-				newKeys[nextCell] = true
-				if len(newKeys) == w.keyCount && curPath.steps+1 < min {
-					min = curPath.steps + 1
+		curPathKey := curPath.ToString()
+		if n, ok := visited[curPathKey]; ok && n <= curPath.steps {
+			continue
+		}
+		visited[curPathKey] = curPath.steps
+
+		curNode := w.graphNodes[curPath.pos]
+		keys := curPath.keys
+
+		if curPath.pos >= 'a' && curPath.pos <= 'z' {
+			keys = map[rune]bool{}
+			for k, v := range curPath.keys {
+				keys[k] = v
+			}
+			keys[curPath.pos] = true
+		}
+
+		if len(keys) == w.keyCount && curPath.steps < min {
+			min = curPath.steps
+		}
+
+		for nextNode, dist := range curNode.linked {
+			if nextNode >= 'A' && nextNode <= 'Z' {
+				lower := 'a' + nextNode - 'A'
+				if !keys[lower] {
 					continue
 				}
-				stack = append(stack, path{
-					steps: curPath.steps + 1,
-					keys:  newKeys,
-					pos:   nextPos,
-				})
-			case nextCell >= 'A' && nextCell <= 'Z':
-				lower := 'a' + nextCell - 'A'
-				if curPath.keys[lower] {
-					stack = append(stack, path{
-						steps: curPath.steps + 1,
-						keys:  curPath.keys,
-						pos:   nextPos,
-					})
-				}
-			default:
-				panic("didn't think we'd get here")
 			}
+
+			stack = append(stack, path{
+				steps: curPath.steps + dist,
+				keys:  keys,
+				pos:   nextNode,
+			})
 		}
 	}
 
