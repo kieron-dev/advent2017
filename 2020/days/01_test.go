ae084d0621248fccfa471b7f5b98dd3c57ca1ebf
Kieron Browne <kieron.browne@gmail.com>
2020-12-01 15:50:01 +0000

    2020/1

---
+++ 2020/days/01_test.go
@@ -0,0 +1,38 @@
+package days_test
+
+import (
+	"os"
+
+	"github.com/kieron-dev/adventofcode/2020/money"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("01", func() {
+	var (
+		moneyWrangler money.Wrangler
+		data          *os.File
+	)
+
+	BeforeEach(func() {
+		moneyWrangler = money.NewWrangler()
+
+		var err error
+		data, err = os.Open("./input01")
+		Expect(err).NotTo(HaveOccurred())
+
+		moneyWrangler.LoadExpenses(data)
+	})
+
+	AfterEach(func() {
+		data.Close()
+	})
+
+	It("part A", func() {
+		Expect(moneyWrangler.ProductFor(2020)).To(Equal(605364))
+	})
+
+	It("part B", func() {
+		Expect(moneyWrangler.Product3For(2020)).To(Equal(128397680))
+	})
+})
---
+++ 2020/money/wrangler_test.go
@@ -0,0 +1,42 @@
+package money_test
+
+import (
+	"io"
+	"strings"
+
+	"github.com/kieron-dev/adventofcode/2020/money"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Wrangler", func() {
+	var (
+		wrangler money.Wrangler
+		data     io.Reader
+	)
+
+	BeforeEach(func() {
+		wrangler = money.NewWrangler()
+
+		data = strings.NewReader(`
+1
+2
+8
+9
+3
+`)
+		wrangler.LoadExpenses(data)
+	})
+
+	It("can find numbers summing to a value", func() {
+		Expect(wrangler.GetSummingTo(12)).To(ConsistOf(3, 9))
+	})
+
+	It("can multiply the sum numbers", func() {
+		Expect(wrangler.ProductFor(12)).To(Equal(27))
+	})
+
+	It("can find three numbers summing to a value", func() {
+		Expect(wrangler.Get3SummingTo(20)).To(ConsistOf(3, 8, 9))
+	})
+})
