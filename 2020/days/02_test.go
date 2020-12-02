2e575d92745428ef6430a8019d5fee1498b4dff9
Kieron Browne <kieron.browne@gmail.com>
2020-12-02 16:23:32 +0000

    Day 2

---
+++ 2020/days/02_test.go
@@ -0,0 +1,34 @@
+package days_test
+
+import (
+	"os"
+
+	"github.com/kieron-dev/adventofcode/2020/password"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("02", func() {
+	var (
+		input   *os.File
+		checker password.Checker
+	)
+
+	BeforeEach(func() {
+		var err error
+		input, err = os.Open("./input02")
+		Expect(err).NotTo(HaveOccurred())
+
+		checker = password.NewChecker()
+
+		checker.Load(input)
+	})
+
+	It("part A", func() {
+		Expect(checker.CorrectCount()).To(Equal(538))
+	})
+
+	It("part B", func() {
+		Expect(checker.CorrectCountNew()).To(Equal(489))
+	})
+})
