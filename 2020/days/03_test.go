eef0d146670f63f3a47ebcdb0756173e6f427e8d
Kieron Browne <kieron.browne@gmail.com>
2020-12-03 09:19:27 +0000

    Day 3

---
+++ 2020/days/03_test.go
@@ -0,0 +1,50 @@
+package days_test
+
+import (
+	"os"
+
+	"github.com/kieron-dev/adventofcode/2020/maps"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("03", func() {
+	var (
+		grid  maps.Cylinder
+		input *os.File
+	)
+
+	BeforeEach(func() {
+		var err error
+		input, err = os.Open("./input03")
+		Expect(err).NotTo(HaveOccurred())
+
+		grid = maps.NewCylinder()
+		grid.Load(input)
+	})
+
+	AfterEach(func() {
+		input.Close()
+	})
+
+	It("does part A", func() {
+		Expect(grid.CountChars(maps.NewCoord(0, 0), maps.NewVector(3, 1), '#')).To(Equal(-1))
+	})
+
+	It("does part B", func() {
+		dirs := []maps.Vector{
+			{X: 1, Y: 1},
+			{X: 3, Y: 1},
+			{X: 5, Y: 1},
+			{X: 7, Y: 1},
+			{X: 1, Y: 2},
+		}
+
+		prod := 1
+		for _, dir := range dirs {
+			prod *= grid.CountChars(maps.NewCoord(0, 0), dir, '#')
+		}
+
+		Expect(prod).To(Equal(-1))
+	})
+})
---
+++ 2020/maps/cylinder_test.go
@@ -0,0 +1,40 @@
+package maps_test
+
+import (
+	"io"
+	"strings"
+
+	"github.com/kieron-dev/adventofcode/2020/maps"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Cylinder", func() {
+	var (
+		input io.Reader
+		grid  maps.Cylinder
+	)
+
+	BeforeEach(func() {
+		grid = maps.NewCylinder()
+
+		input = strings.NewReader(`
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#
+`)
+		grid.Load(input)
+	})
+
+	It("counts the #s in a 3,1 downwards direction from 0,0", func() {
+		Expect(grid.CountChars(maps.NewCoord(0, 0), maps.NewVector(3, 1), '#')).To(Equal(7))
+	})
+})
