debc7c6ff0336705c6551d3cf44e7d413a7b42a5
Kieron Browne <kieron.browne@gmail.com>
2020-12-05 09:56:26 +0000

    Day 5

---
+++ 2020/days/05_test.go
@@ -0,0 +1,37 @@
+package days_test
+
+import (
+	"os"
+
+	"github.com/kieron-dev/adventofcode/2020/seating"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("05", func() {
+	var (
+		data *os.File
+		plan seating.Plan
+	)
+
+	BeforeEach(func() {
+		var err error
+		data, err = os.Open("./input05")
+		Expect(err).NotTo(HaveOccurred())
+
+		plan = seating.NewPlan()
+		plan.Load(data)
+	})
+
+	AfterEach(func() {
+		data.Close()
+	})
+
+	It("part A", func() {
+		Expect(plan.MaxSeatID()).To(Equal(801))
+	})
+
+	It("part B", func() {
+		Expect(plan.MissingSeat()).To(Equal(597))
+	})
+})
