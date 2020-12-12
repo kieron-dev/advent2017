9c105d4a2a1b116b2bfadb67e22e3b4a8f55206b
Kieron Browne <kieron.browne@gmail.com>
2020-12-12 21:24:36 +0000

    Day 12

---
+++ 2020/days/12_test.go
@@ -0,0 +1,39 @@
+package days_test
+
+import (
+	"os"
+
+	"github.com/kieron-dev/adventofcode/2020/navigation"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("12", func() {
+	var (
+		data *os.File
+		ship navigation.Ship
+	)
+
+	BeforeEach(func() {
+		var err error
+		data, err = os.Open("./input12")
+		Expect(err).NotTo(HaveOccurred())
+
+		ship = navigation.NewShip()
+		ship.Load(data)
+	})
+
+	AfterEach(func() {
+		data.Close()
+	})
+
+	It("part A", func() {
+		ship.Move()
+		Expect(ship.ManhattanDistance()).To(Equal(2847))
+	})
+
+	It("part B", func() {
+		ship.MoveNew()
+		Expect(ship.ManhattanDistance()).To(Equal(29839))
+	})
+})
