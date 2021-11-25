9a7b173ed910d0ef7c38ec972e1bf0e47039dd03
Kieron Browne <kbrowne@vmware.com>
2021-11-25 21:10:17 +0000

    2015/10
    Note that efficient string concatenation is the key here

---
+++ 2015/days/10_test.go
@@ -0,0 +1,58 @@
+package days_test
+
+import (
+	"fmt"
+	"strings"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+const dayInput = "3113322113"
+
+var _ = Describe("10", func() {
+	It("does part A", func() {
+		num := dayInput
+
+		for i := 0; i < 40; i++ {
+			num = lookSay(num)
+		}
+
+		Expect(len(num)).To(Equal(329356))
+	})
+
+	It("does part B", func() {
+		num := dayInput
+
+		for i := 0; i < 50; i++ {
+			num = lookSay(num)
+		}
+
+		Expect(len(num)).To(Equal(4666278))
+	})
+})
+
+func lookSay(num string) string {
+	out := strings.Builder{}
+
+	i := 0
+	for i < len(num) {
+		cur := num[i]
+
+		if i == len(num)-1 {
+			out.WriteString("1" + string(cur))
+			break
+		}
+
+		j := 1
+		for num[i+j] == cur {
+			j++
+		}
+
+		out.WriteString(fmt.Sprintf("%d%c", j, cur))
+
+		i += j
+	}
+
+	return out.String()
+}
