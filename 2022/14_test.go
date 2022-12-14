989c794498116e5ce02b2e032613d4cb5375969a
Kieron Browne <kieron.browne@gmail.com>
2022-12-14 20:51:59 +0000

    no need for min/max

--- 2022/14_test.go
+++ 2022/14_test.go
@@ -222,17 +222,3 @@ var _ = Describe("14", func() {
 		Expect(count).To(Equal(23610))
 	})
 })
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
